a975c078c6d31c798143cb9759f8493a1bb19ecd
Eric McKinney <mckinney99@gmail.com>
2017-12-13 18:51:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
     @word3 = params[:word3].to_s
     @word4 = params[:word4].to_s
     @word5 = params[:word5].to_s
-    "#{@word1 @word2 @rod3 @word4 @word5}"
+    "#{@word1 @word2 @word3 @word4 @word5}"
   end
 
 end
